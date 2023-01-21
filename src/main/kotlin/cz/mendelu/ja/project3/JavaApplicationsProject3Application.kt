aa5cd42fd0183ccf5b351ff436230afd12b6b3e2
Eliška Chylíková <xchyliko@mendelu.cz>
2023-01-21 18:03:23 +0100

    Initial commit

---
+++ src/main/kotlin/cz/mendelu/ja/project3/JavaApplicationsProject3Application.kt
@@ -0,0 +1,11 @@
+package cz.mendelu.ja.project3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaApplicationsProject3Application
+
+fun main(args: Array<String>) {
+    runApplication<JavaApplicationsProject3Application>(*args)
+}
---
+++ src/test/kotlin/cz/mendelu/ja/project3/JavaApplicationsProject3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.mendelu.ja.project3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavaApplicationsProject3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
