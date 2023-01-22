34f20ec4e99bb6a8853d6c0b040d638f12b896e6
Eliška Chylíková <xchyliko@mendelu.cz>
2023-01-22 17:25:45 +0100

    Add another unit test for service

--- src/test/kotlin/cz/mendelu/ja/project3/DanceStyleServiceUnitTest.kt
+++ src/test/kotlin/cz/mendelu/ja/project3/DanceStyleServiceUnitTest.kt
@@ -15,6 +15,7 @@ class DanceStyleServiceUnitTest {
     private val danceStyleService = DanceStyleService(danceStyleRepository)
 
     private val danceStyle = DanceStyle(1, "Dancehall", "Jamaica")
+    private val danceStyle2 = DanceStyle(2, "Afro House", "Africa")
 
     @Test
     fun getById_test() {
@@ -27,6 +28,18 @@ class DanceStyleServiceUnitTest {
         // then
         verify(exactly = 1) { danceStyleRepository.findById(1) }
         assertEquals(danceStyle, result)
+    }
 
+    @Test
+    fun getAll_test() {
+        // given
+        every { danceStyleRepository.findAll() } returns listOf(danceStyle, danceStyle2)
+
+        // when
+        val result = danceStyleService.getAll()
+
+        // then
+        verify(exactly = 1) { danceStyleRepository.findAll() }
+        assertEquals(listOf(danceStyle, danceStyle2), result)
     }
 }
